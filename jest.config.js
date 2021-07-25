55768cb8e1cc58dd8a2638e5152041e2c8791329
Gabriel <gabrielfranccelino@gmail.com>
2021-07-24 23:46:11 -0300

    first commit

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: false,
+  collectCoverageFrom: ['src/**/*.js(x)?'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.js'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
+}
